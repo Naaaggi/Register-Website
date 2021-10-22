d19d800cb72a1e89f37fed24f128128b66fd9e5e
Naaaggi <naguigh23@gmail.com>
2021-10-22 19:46:43 +0200

    init

---
+++ script.js
@@ -0,0 +1,36 @@
+function validateForm() {
+    var username = document.forms["contactForm"]["username"].value;
+    var firstname = document.forms["contactForm"]["firstname"].value;
+    var lastname = document.forms["contactForm"]["lastname"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var password = document.forms["contactForm"]["password"].value;
+    var cpassword = document.forms["contactForm"]["password-confirm"].value;
+    
+    if (username == null || username == "") {
+        alert("Please insert a username");
+        return false;
+    }
+    
+    else if (firstname == null || firstname == "") {
+        alert("First Name must be filled out");
+        return false;
+    } else if (lastname == null || lastname == "") {
+        alert("Last Name must be filled out");
+        return false;
+}else if (email == null || email == "") {
+    alert("Email must be filled out");
+    return false;
+
+}
+else if (password == null || password == "") {
+    alert("Password must be filled out");
+    return false;
+
+}
+else if (password !== cpassword ) {
+    alert("The password doesn't match");
+    return false;
+
+}
+
+}
